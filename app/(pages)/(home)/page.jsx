64bfd2ff80a6e04416ad411d3641a21b92996a02
DrewCoding <drewwilliams9090@gmail.com>
2026-02-07 13:33:56 -0800

    General flexbox layout of the store

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -3,9 +3,15 @@ import styles from "./home.module.scss";
 export default function Home() {
   return (
     <main className={styles.page}>
-      <h1 className={styles.title}>Fraunces heading test</h1>
-      <p className={styles.text}>Inter body test</p>
-      <p className={styles.logo}>BOTCH TEST</p>
+      <div className={styles.topBar}>Shop</div>
+      <div className={styles.mainSection}>
+        <div className={styles.leftSection}>
+          <h2>Shop prefs</h2>
+        </div>
+        <div className={styles.rightSection}>
+          <h2>Shop Items</h2>
+        </div>
+      </div>
     </main>
   );
 }
