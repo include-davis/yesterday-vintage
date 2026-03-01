994b853428dc8722be82ceb91357c6994587a0b8
Maisie Wonggit config --global user.email your@email.com <554137@my.cuhsd.org>
2026-03-01 14:21:22 -0800

    final changes

--- app/(pages)/faq/page.jsx
+++ app/(pages)/faq/page.jsx
@@ -24,6 +24,16 @@ export default function FAQ() {
         question="Do you buy clothes from the public?" 
         answer="Yes! All clothes must be 20 years or older and be in good condition. 15 items or less can be conducted virtually through Instagram DM's and 15 or more items can be conducted virtually or by scheduling a time in person." 
       />
+
+      <FreqQ 
+        question="What days of the week are you open?" 
+        answer="We are open every day of the week 12:00 PM - 6:00 PM! Check our Google Calendar on the Shops page to see our holiday schedules." 
+      />
+
+      <FreqQ 
+        question="What days of the week are you open?" 
+        answer="We are open every day of the week 12:00 PM - 6:00 PM! Check our Google Calendar on the Shops page to see our holiday schedules." 
+      />
       </div>
     </main>
   );
