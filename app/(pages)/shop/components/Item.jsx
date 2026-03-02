4b78a4ddaf3215ae3ff6373a5f1d71bfb129a946
DrewCoding <drewwilliams9090@gmail.com>
2026-03-01 18:59:02 -0800

    Mobile implementation

--- app/(pages)/shop/components/Item.jsx
+++ app/(pages)/shop/components/Item.jsx
@@ -5,18 +5,18 @@ import Link from "next/link";
 import styles from "./Item.module.scss";
 
 export function Item({ item }) {
-  const { id, src, title, price } = item;
-  return (
-    <Link href={`/item-page?id=${id}`} className={styles.itemLink}>
-      <div className={styles.item}>
-        <div className={styles.imageContainer}>
-          <Image src={src} alt={title} width={400} height={400} />
-        </div>
-        <div className={styles.infoContainer}>
-          <h3>{title}</h3>
-          <p>${price}</p>
-        </div>
-      </div>
-    </Link>
-  );
+    const { id, src, title, price } = item;
+    return (
+        <Link href={`/item-page?id=${id}`} className={styles.itemLink}>
+            <div className={styles.item}>
+                <div className={styles.imageContainer}>
+                    <Image src={src} alt={title} width={400} height={400} />
+                </div>
+                <div className={styles.infoContainer}>
+                    <h3>{title}</h3>
+                    <p>${price}</p>
+                </div>
+            </div>
+        </Link>
+    );
 }
