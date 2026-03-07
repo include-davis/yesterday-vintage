c5961a406887146156b1399cd0ac885b92d21cd0
alex <174984579+phlangiee@users.noreply.github.com>
2026-03-07 15:27:09 -0800

    formatting the new website

--- app/(pages)/about/page.jsx
+++ app/(pages)/about/page.jsx
@@ -25,20 +25,29 @@ export default function AboutUs() {
         <div className={styles.container}>
             {/** Title */}
             <h1 className={styles.title}>About Us</h1>
+
+            {/** Here's Our Story Section */}
+            <Image src={ourstory} alt={"All staff talking over cake."} className={styles.largeImage} />
+
             <div className={styles.content}>
 
-                {/** Here's Our Story Section */}
-                <div className={styles.row}>
-                    <Image src={ourstory} alt={"All staff talking over cake."} className={styles.largeImage} />
-                    <div className={styles.collumn}>
-                        <h2 className={styles.subtitle}>Here's Our <span className={styles.red}> Story!</span></h2>
-                        <p className={styles.body}>
-                            Yesterday is an owner-operated, four member endeavor. Our names are Michaela Landers, Donbi Kim, Tuan (Stevie) Tat, and John (Jack) Halet. Donbi and Stevie attended UC Davis, and both graduated in 2022, while Jack and Michaela have lived in Davis now for over 3 years.
-                        </p>
-                    </div>
+                {/** Paragraphs */}
+                <div className={styles.body}>
+                    Yesterday is an owner-operated, four member endeavor. 
+                    <br/> <br/>
+                    Our names are Michaela Landers, Donbi Kim, Tuan (Stevie) Tat, and John (Jack) Halet. Donbi and Stevie attended UC Davis, and both graduated in 2022, while Jack and Michaela have lived in Davis now for over 3 years. 
+                    <br/> <br/>
+                    We all began selling vintage at different stages in our lives, but found ourselves all in Davis when we wanted to open a physical storefront. As soon as we stepped foot into the shop, we knew it was the space for us! It was a quick turnaround; our doors opened 30 days after we signed our lease in September of 2023!
+                    <br/> <br/>
+                    Now, we’ve been in Downtown Davis for two years, and feel more grateful than ever to be in a community with so many wonderful fellow business owners and patrons. We’ve been able to engage in awesome collaborations with UC Davis and local student organizations, participate in Davis pop-ups, and now help put together our community-oriented event “Day in Downtown”. 
+                    <br/> <br/>
+                    As we begin a new year, we’re excited to tie up some loose ends in our shop, and bring our complete vision to life! We feel humbled to have made it this far, and we are ever-indebted to our Davis community. Whether you have been following us from the very beginning, or you just discovered us today, Yesterday wouldn’t be possible without you all. 
+                    <br/> <br/>
+                    Love, <br/>
+                    Donbi, Michaela, Stevie, & Jack
                 </div>
 
-                {/** Staff Section */}
+                {/** Staff */}
                 <div className={styles.pictureRow}>
                     {people.map((person) => (
                         <div key={person.name} className={styles.person}>
@@ -48,37 +57,12 @@ export default function AboutUs() {
                     ))}
                 </div>
 
-                {/** Paragraphs Section */}
-                <div className={styles.row}>
-                    <p className={styles.smallBody}>
-                        We all began selling vintage at different stages in our lives, but found ourselves all in Davis when we wanted to open a physical storefront. As soon as we stepped foot into the shop, we knew it was the space for us! It was a quick turnaround; our doors opened 30 days after we signed our lease in September of 2023!
-                    </p>
-                    <Image className={styles.mediumImage} src={paragraphs} alt={"Two stylish people posing in Yesterday Vintage."}  />
-                </div>
-                <div className={styles.invert}>
-                <div className={styles.row}> 
-                    
-                    <Image className={styles.mediumImage} src={paragraphs} alt={"Two stylish people posing in Yesterday Vintage."} />
-                    <p className={styles.body}>
-                        Now, we’ve been in Downtown Davis for two years, and feel more grateful than ever to be in a community with so many wonderful fellow business owners and patrons. We’ve been able to engage in awesome collaborations with UC Davis and local student organizations, participate in Davis pop-ups, and now help put together our community-oriented event “Day in Downtown”. 
-                    </p>
-                    
-                </div>
-                </div>
-
-                {/** Closing Section */}
-                <div className={styles.invert}>
+                {/** Other Images */}
                 <div className={styles.pictureRow}>
-                    <Image className={styles.smallImage} src={closing1} alt={"All the staff smiling in one photo."} />
-                    <Image className={styles.smallImage} src={closing2} alt={"All staff talking over cake."} />
-                    <Image className={styles.smallImage} src={closing3} alt={"All the staff smiling in one photo."} />
-                    <Image className={styles.smallImage} src={closing3} alt={"All the staff smiling in one photo."} />
-                </div>
-                <div className={styles.closingMessage}>
-                    As we approach the new year, we’re excited to tie up some loose ends in our shop, and bring our complete vision to life! We feel humbled to have made it this far, and we are ever-indebted to our Davis community. Whether you have been following us from the very beginning, or you just discovered us today, Yesterday wouldn’t be possible without you all. 
-                    <br /> <br />
-                    Love, Donbi, Michaela, Stevie, & Jack 
-                </div>
+                    <Image className={styles.image} src={closing1} alt={"All the staff smiling in one photo."} />
+                    <Image className={styles.image} src={closing2} alt={"All staff talking over cake."} />
+                    <Image className={styles.image} src={closing3} alt={"All the staff smiling in one photo."} />
+                    <Image className={styles.image} src={closing3} alt={"All the staff smiling in one photo."} />
                 </div>
 
             </div>
